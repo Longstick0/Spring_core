ca4d95a185693846ce68f7e68e21b04c5d6c1d11
jango <789456jang@naver.com>
2021-07-20 21:02:12 +0900

    Use Lombok and Fix the Test

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,21 +6,30 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final이 붙은 객체 -> 생성자로 만들어줌
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
+//    //생성자가 하나기 때문에 @Autowired는 생략 가능, @RequiredArgsConstructor로 인해 생성자가 만들어졌으므로 주석처리
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
     }
-
-
 
     //    @Autowired //(required = false) 선택적 의존 관계
 //    // @Autowired의 기본 동작은 주입할 대사이 없으면 오류가 발생한다. 주입할 대상이 없어도 동작하게 하려면 required를
@@ -41,14 +50,6 @@ public class OrderServiceImpl implements OrderService{
 //    }
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-
-    @Override
-    public Order createOrder(Long memberId, String itemName, int itemPrice) {
-        Member member = memberRepository.findById(memberId);
-        int discountPrice = discountPolicy.discount(member, itemPrice);
-
-        return new Order(memberId, itemName, itemPrice, discountPrice);
-    }
 
     //테스트 용도
     public MemberRepository getMemberRepository() {
